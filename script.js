006fbed5952ef765622702859fdc32e920ed3d20
tauheed0110 <tauheed0110@gmail.com>
2024-12-21 13:50:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // your code here
+const button = document.getElementById('button');
+const url = document.getElementById('url');
+
+button.addEventListener('click', (e)=>{
+    e.preventDefault();
+    url.textContent = location.href;
+})
